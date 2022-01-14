3f146a27b36b63b40a43325360ead991971cd287
jadogbpalmer6 <jeandedieuuwizeye6@gmail.com>
2022-01-14 18:44:06 +0200

    new remote commit

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from allauth.socialaccount.providers.twitter.views import TwitterOAuthAdapter
 
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from rest_auth.social_serializers import TwitterLoginSerializer
 
 class FacebookLogin(SocialLoginView):
     adapter_class = FacebookOAuth2Adapter
@@ -20,4 +21,6 @@ class TwitterLogin(SocialLoginView):
     adapter_class = TwitterOAuthAdapter
     callback_url = "http://127.0.0.1:8000/accounts/twitter/login/callback/"
     
-
+class TwitterLogin(SocialLoginView):
+    serializer_class = TwitterLoginSerializer
+    adapter_class = TwitterOAuthAdapter
